d57f13186c5ad2fce0d0bb90aed71837b04e65b0
Sunntle <taikute012532@gmail.com>
2023-09-25 21:54:16 +0700

    FIx

--- src/web3.js
+++ src/web3.js
@@ -1,4 +1,7 @@
 import Web3 from "web3";
-const web3 = new Web3(window.ethereum || "ws://192.168.1.12:7545");
+const web3 = new Web3(
+  window.ethereum ||
+    new Web3.providers.WebsocketProvider("ws://192.168.1.12:7545")
+);
 
 export default web3;
