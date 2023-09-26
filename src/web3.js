9a0d9e0675c1e39b72e89d38a3b4cac158016bc7
Sunntle <taikute012532@gmail.com>
2023-09-26 16:10:25 +0700

    SOme thing change

--- src/web3.js
+++ src/web3.js
@@ -1,9 +1,10 @@
 import Web3 from "web3";
+
 const web3 = new Web3(
   window.ethereum ||
     new Web3.providers.WebsocketProvider(
       process.env.NODE_ENV === "production"
-        ? "wss://192.168.1.12:7545"
+        ? `wss://mainnet.infura.io/ws/v3/${process.env.PROJECT_ID}`
         : "ws://192.168.1.12:7545"
     )
 );
