0b72684b84e305765546c01dc69ecb302e623baf
Sunntle <taikute012532@gmail.com>
2023-09-26 15:20:45 +0700

    Fix provider

--- src/web3.js
+++ src/web3.js
@@ -1,7 +1,11 @@
 import Web3 from "web3";
 const web3 = new Web3(
   window.ethereum ||
-    new Web3.providers.WebsocketProvider("ws://192.168.1.12:7545")
+    new Web3.providers.WebsocketProvider(
+      process.env.NODE_ENV === "production"
+        ? "wss://192.168.1.12:7545"
+        : "ws://192.168.1.12:7545"
+    )
 );
 
 export default web3;
