3c8fe4e8316a72356f483c2ff1303b9874e63aa7
Sunntle <taikute012532@gmail.com>
2023-09-28 10:38:30 +0700

    something change

--- src/web3.js
+++ src/web3.js
@@ -1,7 +1,7 @@
 import Web3 from "web3";
 const web3 = new Web3(
   window.ethereum ||
-    new Web3.providers.HttpProvider(
+    new Web3.providers.WebsocketProvider(
       `wss://mainnet.infura.io/ws/v3/${process.env.REACT_APP_PROJECT_ID}`
     )
 );
